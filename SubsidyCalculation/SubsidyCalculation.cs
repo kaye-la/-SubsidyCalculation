abeb198a93252ac0a9a766490a902dd3314682f9
Kaye <qwer>
2021-04-07 14:44:32 +0300

    init

---
+++ SubsidyCalculation/SubsidyCalculation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubsidyCalculation
+{
+    public class SubsidyCalculation : ISubsidyCalculation
+    {
+        public event EventHandler<string> OnNotify;
+        public event EventHandler<Tuple<string, Exception>> OnException;
+
+        public Charge CalculateSubsidy(Volume volumes, Tariff tariff)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
